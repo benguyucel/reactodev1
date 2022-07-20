5de3fa71b4163e501e351575730872b695923dc6
benguyucel <yucelbengu8@gmail.com>
2022-07-20 15:44:42 +0300

    ****

---
+++ app.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const getData = async(userId) => {
+    const user = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const posts = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+    const userData = {...user.data, posts: posts.data }
+    console.log(userData)
+}
+
+export default getData;
