f91878788a5dc24ce4318f83e7c3508a4ad020da
benguyucel <yucelbengu8@gmail.com>
2022-07-20 15:49:55 +0300

    ---

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const getData = async(userId) => {
     const user = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
     const posts = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
     const userData = {...user.data, posts: posts.data }
-    console.log(userData)
+    return userData;
 }
 
 export default getData;
